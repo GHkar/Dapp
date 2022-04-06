2bbf9d779a4c3216b857bc1ec3cac1111a21f448
KimAeri <52875499+GHkar@users.noreply.github.com>
2022-04-06 11:57:02 +0900

    블록체인 인 액션 - Accounts.sol 코드 업로드

--- Accounts.sol
+++ Accounts.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13; // (1) version pragma
+// (2) Declare Contract
+contract AccountDemo {
+        // (3) Declare state word
+        address public whoDeposited;
+        uint public depositAmt;
+        uint public accountBalance;
 
+        // (4) producer
+        // (5) Declare method
+        function deposit() public payable {
+            whoDeposited = msg.sender;
+            depositAmt = msg.value;
+            accountBalance = address(this).balance;
+        }
+}
